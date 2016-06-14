ab6205a72c7fee35226cec6385fb2ef8da52cf7d
arshad khan <arshdkhn1@gmail.com>
2016-06-14 12:27:10 +0000

    updating jade.js file

--- jade.js
+++ jade.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var app = express();
-var path = require("path");
 app.set('views', process.argv[3]);
-console.log(process.argv[3])
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 app.get('/home',function(req, res){
    res.render('index', { date : new Date().toDateString() }); 
 });
